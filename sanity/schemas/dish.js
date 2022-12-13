05992c8653b570bd20380604250a30b54b62a3a1
Fred Lefebvre <fred@MacBook-Air-de-Christine.local>
2022-12-13 13:04:01 -0500

    Integrated sanity cms and modified schemas

---
+++ sanity/schemas/dish.js
@@ -0,0 +1,31 @@
+import {defineType} from 'sanity'
+
+export default defineType({
+  name: 'dish',
+  title: 'Dish',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Dish name',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'short_description',
+      type: 'string',
+      title: 'Short description',
+      validation: (Rule) => Rule.max(200),
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Dish image',
+    },
+    {
+      name: 'price',
+      type: 'number',
+      title: 'Dish price',
+    },
+  ],
+})
